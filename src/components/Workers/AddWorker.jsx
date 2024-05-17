ad94c5629249e6164a11b79167349b333c48738a
Sevval Sayın <ssayin0498@gmail.com>
2024-05-17 17:57:47 +0300

    fragments,portals,refs

--- src/components/Workers/AddWorker.jsx
+++ src/components/Workers/AddWorker.jsx
@@ -1,51 +1,84 @@
-import { useState } from "react";
+import { useState, Fragment, useRef } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 
 const AddWorker = (props) => {
-  const [enteredWorkerName, setEnteredWorkerName] = useState("");
-  const [enteredWage, setEnteredWage] = useState("");
   const [error, setError] = useState();
+  const nameInputRef = useRef();
+  const wageInputRef = useRef();
 
   const minimumWage = 5000;
 
   const addWorkerHandler = (e) => {
     e.preventDefault();
-    if (enteredWorkerName.trim().length === 0) {
+const enteredName=nameInputRef.current.value;
+const enteredWage=wageInputRef.current.value;
+
+    if (nameInputRef.current.value.trim().length === 0) {
       setError({
         title: "İsim Alanı Zorunludur!",
-        message: "Lütfen Bir İsim Giriniz!",
+        message: "Lütfen bir isim giriniz.",
       });
       return;
     }
 
-    if (+enteredWage < minimumWage) {
+    if (+wageInputRef.current.value < minimumWage) {
       setError({
         title: "Maaş Alanı Zorunludur!",
-        message: `Lütfen ${minimumWage} Değerinden Büyük Bir Tutar Giriniz`,
+        message: `Lütfen ${minimumWage} değerinden büyük bir maaş değeri giriniz.`,
       });
       return;
     }
-
     props.setWorkers((prevState) => [
       {
         id: Math.floor(Math.random() * 1000),
-        name: enteredWorkerName,
+        name: enteredName,
         wage: enteredWage,
       },
       ...prevState,
     ]);
-
-    setEnteredWorkerName("");
-    setEnteredWage("");
+    nameInputRef.current.value="";
+    wageInputRef.current.value="";
   };
 
   const errorHandler = () => {
     setError(null);
   };
   return (
-    <div>
+    <Fragment>
+      {error && <ErrorModal onConfirm={errorHandler} error={error} />}
+      <Card className="mt-10">
+        <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
+          <label htmlFor="name" className="font-medium">
+            Çalışan İsmi
+          </label>
+          <input
+            type="text"
+            className="max-w-[40rem] w-full mx-auto border p-2"
+            placeholder="Çalışan ismi yazınız"
+            id="name"
+
+            ref={nameInputRef}
+          />
+          <label htmlFor="wage" className="font-medium">
+            Maaş Miktarı
+          </label>
+          <input
+            type="number"
+            className="max-w-[40rem] w-full mx-auto border p-2"
+            placeholder="Maaş miktarı yazınız"
+            id="wage"
+
+            ref={wageInputRef}
+          />
+          <Button className="mt-2" type="submit">
+            Ekle
+          </Button>
+        </form>
+      </Card>
+    </Fragment>
+    /*     <>
       {error && <ErrorModal onConfirm={errorHandler} error={error} />}
       <Card className="mt-10">
         <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
@@ -76,7 +109,7 @@ const AddWorker = (props) => {
           </Button>
         </form>
       </Card>
-    </div>
+    </> */
   );
 };
 
